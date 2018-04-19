3acf1ca4b1343721e48973568cc7393b7036912a
Farha-Mahzbin <zabin.farha.01@gmail.com>
2018-04-19 09:45:31 +0600

    adding showTash function[show]

--- todo_list.cpp
+++ todo_list.cpp
@@ -6,16 +6,7 @@ struct todo
 	char time[100], task[100];
 };
 
-void addTask()
+void showtask()
 {
-    ofstream fileWrite("tasks.txt", ios::app | ios::binary);
-	struct todo myTask;
-	cout << "Insert task: ";
-	cin >> myTask.task;
-	cout << "Insert time: ";
-	cin >> myTask.time;
 
-	fileWrite.write((char *) &myTask, sizeof(myTask));
-	cout << "Added!\n";
-	fileWrite.close();
 }
